1b3a6379d94b968fdb32ad760d25668685e9533b
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2025-02-17 14:59:15 +0300

    added routes  for the page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import './App.css'
 import Header from './components/Header/Header'
 import MainNav from './components/MainNav/MainNav'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Trending from './pages/Trending/Trending'
+import Movies from './pages/Movies/Movies'
+import Series from './pages/Series/Series'
+import Search from './pages/Search/Search'
 
 function App() {
   return (
-    <>
-       <Header />
-       <div className='app'>
-        <MainNav />
-       </div>
-    </>
+    <Router>
+      <Header />
+      <div className='app'>
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Trending />} />
+            <Route path='/movies' element={<Movies />} />
+            <Route path='/series' element={<Series />} />
+            <Route path='/search' element={<Search />} />
+          </Routes>
+        </div>
+      </div>
+      <MainNav />
+    </Router>
   )
 }
 
-export default App
+export default App;
