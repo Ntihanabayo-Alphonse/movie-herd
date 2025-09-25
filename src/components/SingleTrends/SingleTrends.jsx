2bb70d824385b23a18cd15d84db60d00b7dee2fd
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2025-09-25 14:06:09 +0300

    wrapped the subTitles in a div parent

--- src/components/SingleTrends/SingleTrends.jsx
+++ src/components/SingleTrends/SingleTrends.jsx
@@ -6,9 +6,10 @@ const SingleTrends = ({id, poster, title, date, vote_average, media_type}) => {
     <div className='single_movie'>
         <img className='poster' src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
         <h3 className='title'>{title}</h3>
-        <span className='sub_title'>{media_type === "tv" ? "TV Series" : "Movie"}</span>
-        <span className='sub_title'>{date}</span>
-        <span className='sub_title'>⭐{vote_average}</span>
+        <div className='subTitle'>
+            <span>{media_type === "tv" ? "TV Series" : "Movie"}</span>
+            <span>{date}</span>
+        </div>
     </div>
   )
 }
